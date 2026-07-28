b2d2d590eceee1be1c1b1b4ed90f040b03985f6b
Anthony Wan <ayhwan@gmail.com>
2026-07-29 02:18:19 +0800

    Correction to urls

--- learning_templates/urls.py
+++ learning_templates/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from basic_app import views
 
